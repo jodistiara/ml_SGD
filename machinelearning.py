3a3130460bef5b269363f980a6c55e2fdb71554e
jodistiara <jodistiara30@gmail.com>
2018-03-06 16:52:49 +0700

    data validation

--- machinelearning.py
+++ machinelearning.py
@@ -4,11 +4,14 @@ import matplotlib.pyplot as plt
 import math
 
 #DEFINE INITIAL VARIABLES
-teta = np.array([0.3,0.6,0.9,0.2])
-bias = np.array([0.5])
-alpha = 0.1
+newteta = np.array([0.3,0.6,0.9,0.2])
+newbias = np.array([0.5])
+teta = newteta.copy()
+bias = newbias.copy()
+alpha = 0.8
 epoch = 60
-error = np.zeros(epoch)
+trainerror = np.zeros(epoch)
+testerror = np.zeros(epoch)
 localerror = 0.000000000000000000
 
 
@@ -23,10 +26,10 @@ data[4] = data[4].astype('int64')
 
 #SPLIT DATA
 traindata = np.vstack([data.iloc[0:40, 0:4], data.iloc[60:100, 0:4]])
-trainclass = np.hstack([data.iloc[0:40, 4], data.iloc[60:100, 4]])
+trainlabel = np.hstack([data.iloc[0:40, 4], data.iloc[60:100, 4]])
 
 testdata = data.iloc[40:60, 0:4].values
-testclass = data.iloc[40:60, 4].values
+testlabel = data.iloc[40:60, 4].values
 
 
 #DEFINE FUNCTION
@@ -51,29 +54,34 @@ def local_error(a,b):
 def delta(g, y, x):
     return (2*(g-y)*(1-g)*g*x)
 
-#TRAIN
+
 for n in range(epoch):
+
+    #TRAIN
     totalerror = 0
     for i in range(len(traindata[0])):
-        
+
+        teta = newteta.copy()
+        bias = newbias.copy()
+
         h = fungsiH(traindata[i,:],teta,bias)
         sigm = sigmoid(h)
         pred = predict(sigm)
 
-        localerror = local_error(sigm, trainclass[i])
+        localerror = local_error(sigm, trainlabel[i])
         # print("teta: ", teta)
         # print("bias: ", bias)
         dteta = np.zeros(4)
         dbias = np.zeros(1)
 
         for j in range(len(dteta)):
-            dteta[j] = delta(sigm, trainclass[i], traindata[i,j])
-        dbias = np.array(delta(sigm, trainclass[i], 1))
+            dteta[j] = delta(sigm, trainlabel[i], traindata[i,j])
+        dbias = np.array(delta(sigm, trainlabel[i], 1))
 
         for k in range(len(teta)):
-            teta[k] = teta[k] - (alpha*dteta[k])
+            newteta[k] = teta[k] - (alpha*dteta[k])
 
-        bias = bias - (alpha*dbias)
+        newbias = bias - (alpha*dbias)
 
         # print("h: ", h)
         # print("sigmoid: ", sigm)
@@ -85,15 +93,40 @@ for n in range(epoch):
         # print("---------------------------------")
         totalerror = totalerror + localerror
 
-    error[n] = totalerror
+    trainerror[n] = totalerror
     # print("error[", n, "]: ", error[n])
 
-plt.clf()
+    #TEST
+    totalerror = 0
+    for i in range(len(testdata[0])):
+        
+        h = fungsiH(testdata[i,:],newteta,newbias)
+        sigm = sigmoid(h)
+        pred = predict(sigm)
+
+        localerror = local_error(sigm, testlabel[i])
+        # print("teta: ", teta)
+        # print("bias: ", bias)
+        # print("h: ", h)
+        # print("sigmoid: ", sigm)
+        # print("predict: ", pred)
+        # print("fact: ", fact)
+        # print("local error: ", localerror)
+        # print("dteta: ", dteta)
+        # print("dbias: ", dbias)
+        # print("---------------------------------")
+        totalerror = totalerror + localerror
+
+    testerror[n] = totalerror
+
 
 x = np.arange(epoch)
-y = error.copy()
+y1 = trainerror.copy()
+y2 = testerror.copy()
 
-plt.plot(x,y)
+plt.figure(figsize=(16,8))
+plt.plot(x,y1, color="green")
+plt.plot(x,y2, color="red")
 plt.xlabel("Epoch")
 plt.ylabel("Error")
 plt.title("Grafik Error per Epoch")
